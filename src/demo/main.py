befbc2d5e6a50548ecf2ecfedfeffd78566e845f
Evan Duffield <duffieldevan@gmail.com>
2024-02-01 17:10:17 -0700

    Demo for Feb 1st

---
+++ src/demo/main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    with open('report.json') as json_file:
+        data = json.load(json_file)
+
+    return render_template('index.html', report_data=data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
